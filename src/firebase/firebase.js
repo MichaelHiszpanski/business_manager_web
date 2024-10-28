60243ce4a529b2f878535322f77efa3f69eae953
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-10-28 22:44:40 +0000

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 if (!firebase.apps.length) {
