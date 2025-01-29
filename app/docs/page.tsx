69b31f2a54180ff8cfc2b59ec2c592c3611b2d22
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2025-01-29 22:37:31 +0000

    selected-none

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -36,13 +36,13 @@ const Docs: NextPage = ({}) => {
               className=" p-8 rounded-xl  hover:scale-105 transition-transform duration-150 ease-in-out
                shadow-lg shadow-white bg-colorFive relative border border-colorSix "
             >
-              <h3 className=" text-xl font-semibold text-colorSix font-orbitron_variable">
+              <h3 className=" text-xl font-semibold text-colorSix font-orbitron_variable select-none">
                 {post.title}
               </h3>
-              <p className=" mt-4 text-sm text-colorSeven font-mono mb-2">
+              <p className=" mt-4 text-sm text-colorSeven font-mono mb-2 select-none">
                 {post.meta}
               </p>
-              <p className=" absolute bottom-3 right-8 font-orbitron_variable text-sm text-white">
+              <p className=" absolute bottom-3 right-8 font-orbitron_variable text-sm text-white select-none">
                 Read more...
               </p>
             </Link>
