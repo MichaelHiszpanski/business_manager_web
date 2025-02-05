050bf4d8f3048e8dac140b717a206335c6b58ecc
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2025-02-05 22:33:54 +0000

    fix outside click

--- src/components/service-side-bar/ServiceSideBar.tsx
+++ src/components/service-side-bar/ServiceSideBar.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Link from "next/link";
+import useOutsideClick from "@/utils/helpers/useOutsideClick";
 
 interface SidebarProps {
   posts: { title: string; slug: string }[];
@@ -9,7 +10,8 @@ interface SidebarProps {
 const ServicesSideBar: React.FC<SidebarProps> = ({ posts }) => {
   const [openDropdown, setOpenDropdown] = useState<string | null>(null);
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
-
+  const navRef = useRef<HTMLDivElement>(null);
+  useOutsideClick(navRef, () => setIsExpanded(false));
   const handleDropdownClick = (slug: string) => {
     setOpenDropdown(openDropdown === slug ? null : slug);
   };
@@ -23,8 +25,8 @@ const ServicesSideBar: React.FC<SidebarProps> = ({ posts }) => {
   };
 
   return (
-    <aside className=" w-min  md:w-1/5 bg-transparent md:p-6 p-2 fixed left-0 top-[150px] cursor-pointer z-50">
-      <nav>
+    <aside className=" w-max  md:w-1/5 bg-transparent md:p-6 p-2 fixed left-0 top-[150px] cursor-pointer z-50">
+      <nav ref={navRef}>
         <h2
           className="text-2xl md:text-3xl font-bold mb-4 text-colorFour cursor-pointer select-none"
           onClick={handleHeadingClick}
@@ -39,7 +41,7 @@ const ServicesSideBar: React.FC<SidebarProps> = ({ posts }) => {
                   onClick={() => handleDropdownClick(post.slug)}
                   className="cursor-pointer"
                 >
-                  <p className="mt-4 text-[11px] md:text-sm  text-colorFour hover:font-semibold">
+                  <p className="mt-4 text-[16px] md:text-sm  text-colorFour hover:text-white">
                     {post.title}
                   </p>
                 </div>
@@ -47,7 +49,7 @@ const ServicesSideBar: React.FC<SidebarProps> = ({ posts }) => {
                   <div className="mt-2 ml-4">
                     <Link href={`/docs/${post.slug}`}>
                       <p
-                        className="mt-4 text-[9px] md:text-sm text-colorTwo hover:font-semibold"
+                        className="mt-4 text-[14px] md:text-sm text-colorTwo hover:font-semibold hover:text-colorSix"
                         onClick={handleLinkClick}
                       >
                         Read more
