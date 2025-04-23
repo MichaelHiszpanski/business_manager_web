be3864eacd457b39b0ce5abb5ad09061e22e6f5a
MichaelHiszpanski <hiszpan4444@googlemail.com>
2025-04-23 16:17:49 +0100

    cleaning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import NavigationBar from "@/components/NavigationBar/NavigationBar";
 import Footer from "@/components/footer/Footer";
-
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
